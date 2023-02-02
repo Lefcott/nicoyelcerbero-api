3f2f6d0bbea6719173c65dcad3b1626d4b96748b
lefcott <lefcott@hotmail.com>
2023-02-02 13:43:07 -0300

    added log middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,13 @@ import ticketPaymentRoutes from "./ticketPayments";
 import payentEvents from "./paymentEvents";
 import showRoutes from "./shows";
 import cronJobRoutes from "./cron-jobs";
+import requestLoggerMiddleware from "./middlewares/log";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLoggerMiddleware);
 
 app.use(ticketPaymentRoutes);
 app.use(payentEvents);
---
+++ src/routes/middlewares/log.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+
+const requestLoggerMiddleware = (req: Request, res: Response, next) => {
+  const date = new Date().toLocaleString();
+  const start = Date.now();
+
+  const write = res.write;
+  const end = res.end;
+  let body = "";
+  // @ts-ignore
+  res.write = (...args) => {
+    body += args[0];
+    // @ts-ignore
+    write.apply(res, args);
+  };
+
+  // @ts-ignore
+  res.end = (...args) => {
+    if (args[0]) {
+      body += args[0];
+    }
+
+    console.log("\x1b[1m\n" + "=".repeat(50) + "\x1b[0m");
+    console.log(
+      `\x1b[1m[${date}] ${req.method} ${
+        req.url.substring(0, req.url.indexOf("/")) || req.url
+      }\x1b[0m`
+    );
+    console.log("\x1b[1m" + "-".repeat(50) + "\x1b[0m");
+    console.log("\x1b[33m" + "Headers:" + "\x1b[0m");
+    console.log(JSON.stringify(req.headers, null, 2));
+    console.log("\x1b[33m" + "\nBody:" + "\x1b[0m");
+    console.log(JSON.stringify(req.body, null, 2));
+    console.log("\x1b[33m" + "\nParams:" + "\x1b[0m");
+    console.log(JSON.stringify(req.params || {}, null, 2));
+    console.log("\x1b[33m" + "\nQuery:" + "\x1b[0m");
+    console.log(JSON.stringify(req.query, null, 2));
+
+    const timeTaken = Date.now() - start;
+
+    console.log("\x1b[32m" + "\nResponse:" + "\x1b[0m");
+    console.log(`\x1b[32mStatus: ${res.statusCode}\x1b[0m`);
+    console.log(`\x1b[32mBody:\n${body}\x1b[0m`);
+    console.log(`\x1b[32mTime taken: ${timeTaken}ms\x1b[0m`);
+    console.log("\x1b[1m" + "=".repeat(50) + "\x1b[0m" + "\n");
+
+    // @ts-ignore
+    end.apply(res, args);
+  };
+
+  next();
+};
+
+export default requestLoggerMiddleware;
