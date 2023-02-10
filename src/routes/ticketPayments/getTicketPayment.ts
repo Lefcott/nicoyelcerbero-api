c0ee5fad86012aa2fa7d80055dd71699d7a1fddb
lefcott <lefcott@hotmail.com>
2023-02-10 03:25:22 -0300

    validate object id

--- src/routes/ticketPayments/getTicketPayment.ts
+++ src/routes/ticketPayments/getTicketPayment.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import { isValidObjectId } from "mongoose";
 import TicketPayment from "../../models/ticketPayment";
 
 const router = express.Router();
 
 router.get("/:ticketPaymentId", async (req, res) => {
   const { ticketPaymentId } = req.params;
+
+  if (!isValidObjectId(ticketPaymentId)) {
+    return res.json(null);
+  }
+
   const ticketPayment = await TicketPayment.findById(ticketPaymentId);
 
   res.json(ticketPayment);
