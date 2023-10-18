ca95a3a7e001bf07f2622e632b83afa0d07464bc
lefcott <lefcott@hotmail.com>
2023-10-18 18:20:44 -0300

    add createdAt field

--- src/models/opinion.ts
+++ src/models/opinion.ts
@@ -2,10 +2,12 @@ import mongoose, { Schema } from "mongoose";
 
 export interface OpinionInterface {
   text: string;
+  createdAt: Date;
 }
 
 const opinionSchema = new Schema<OpinionInterface>({
   text: { type: String, required: true },
+  createdAt: { type: Date, default: () => new Date() },
 });
 
 const Opinion = mongoose.model("Opinion", opinionSchema, "opinions");
