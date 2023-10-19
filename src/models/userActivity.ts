b51f39acde2c2cdccb32fd4abe287a3162e55ab4
lefcott <lefcott@hotmail.com>
2023-10-19 01:51:37 -0300

    user activity collection and elapsed time

---
+++ src/models/userActivity.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface UserActivityInterface {
+  pageVisitId: string;
+  timeElapsed: number; // Time the user spent in the page in seconds
+  createdAt: Date;
+}
+
+const userActivitySchema = new Schema<UserActivityInterface>({
+  pageVisitId: { type: String, required: true },
+  timeElapsed: { type: Number, default: 0 },
+  createdAt: { type: Date, default: () => new Date() },
+});
+
+const UserActivity = mongoose.model(
+  "UserActivity",
+  userActivitySchema,
+  "userActivities"
+);
+
+export default UserActivity;
--- src/sockets/showDetailsPage.ts
+++ src/sockets/showDetailsPage.ts
@@ -1,5 +1,29 @@
 import io from ".";
+import UserActivity from "../models/userActivity";
 
 const showDetailsPageSocket = io.of("showDetailsPage");
+
+showDetailsPageSocket.on("connection", async (connection) => {
+  // @ts-ignore
+  const { pageVisitId } = connection.request._query;
+
+  if (pageVisitId) {
+    await new UserActivity({ pageVisitId }).save().catch((error) => {
+      console.error("error saving user activity", error);
+    });
+  }
+
+  connection.on("disconnect", async () => {
+    if (pageVisitId) {
+      const userActivity = await UserActivity.findOne({ pageVisitId });
+      if (userActivity) {
+        userActivity.timeElapsed = Math.floor(
+          (+new Date() - +userActivity.createdAt) / 1000
+        );
+        await userActivity.save();
+      }
+    }
+  });
+});
 
 export default showDetailsPageSocket;
